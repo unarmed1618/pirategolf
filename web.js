f11a82a1f4a0ede31f9a1162c2bdc2c50df5da02
John Darrow <darrowj08@students.ecu.edu>
2014-01-14 15:19:11 -0500

    added docs

--- web.js
+++ web.js
@@ -1,6 +1,8 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
+var pg = require("pg");
+
 var app = express();
 
 app.use(logfmt.requestLogger());
@@ -12,4 +14,11 @@ app.get('/', function(req, res) {
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
 	console.log("Listening on " + port);
+    });
+pg.connect(process.env.DATABASE_URL, function(err,client) {
+	var query = client.query('SELECT * FROM your_table');
+	
+	query.on('row', function(row) {
+		console.log(JSON.stringify(row));
+	    });
     });

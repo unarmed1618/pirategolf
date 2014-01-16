6a0c81234cb6ad39e4f96aa2f8c42c505f1bded6
John Darrow <darrowj08@students.ecu.edu>
2014-01-16 14:34:18 -0500

    Testing Serverside functionality

--- web.js
+++ web.js
@@ -18,7 +18,8 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
 	console.log("Listening on " + port);
     });
-pg.connect(process.env.DATABASE_URL, function(err,client) {
+pg.connect("postgres://zfaagftogdvhjz:pcXlJD1bP9AygIM7ivINuDOHvS@ec2-184-73-194-196.compute-1.amazonaws.com:5432/dfcvk500ed0il4", function(err,client) {
+	if(err) throw err;
 	var query = client.query('SELECT * FROM GolfRounds');
 	
 	query.on('row', function(row) {

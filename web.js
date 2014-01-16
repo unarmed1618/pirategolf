eaf24ff7d4a07c2252199c0022ab500d4535faa6
John Darrow <darrowj08@students.ecu.edu>
2014-01-16 14:48:52 -0500

    added /read

--- web.js
+++ web.js
@@ -2,6 +2,10 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var pg = require("pg");
+var conString = "postgres://zfaagftogdvhjz:pcXlJD1bP9AygIM7ivINuDOHvS@ec2-184-73-194-196.compute-1.amazonaws.com:5432/dfcvk500ed0il4"
+    var client = new pg.Client(conString);
+client.connect();
+
 
 var app = express();
 
@@ -11,15 +15,19 @@ app.get('/', function(req, res) {
 	res.send('Hello World!');
     });
 app.get('/read', function(req, res) {
+	
 	res.send('A Reader will go here');
+	res.send(client.query("SELECT * FROM GolfRounds"));
+
     });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
 	console.log("Listening on " + port);
     });
-pg.connect("postgres://zfaagftogdvhjz:pcXlJD1bP9AygIM7ivINuDOHvS@ec2-184-73-194-196.compute-1.amazonaws.com:5432/dfcvk500ed0il4", function(err,client) {
-	if(err) throw err;
+
+pg.connect(conString, function(err ,client) {
+	//	if(err) throw err;
 	var query = client.query('SELECT * FROM GolfRounds');
 	
 	query.on('row', function(row) {

e774448161f7eb100bc29814b4eb0354b74b7055
John Darrow <darrowj08@students.ecu.edu>
2014-01-16 14:03:27 -0500

    Trying to add database powers

--- web.js
+++ web.js
@@ -10,13 +10,16 @@ app.use(logfmt.requestLogger());
 app.get('/', function(req, res) {
 	res.send('Hello World!');
     });
+app.get('/read' function(req, res) {
+	res.send('A Reader will go here');
+    });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
 	console.log("Listening on " + port);
     });
 pg.connect(process.env.DATABASE_URL, function(err,client) {
-	var query = client.query('SELECT * FROM your_table');
+	var query = client.query('SELECT * FROM GolfRounds');
 	
 	query.on('row', function(row) {
 		console.log(JSON.stringify(row));

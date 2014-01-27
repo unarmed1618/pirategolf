0606c5e6822e89203778e73d8a2f4c1868943897
John Darrow <darrowj08@students.ecu.edu>
2014-01-27 12:13:03 -0500

    Testing multiline

--- web.js
+++ web.js
@@ -2,8 +2,8 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var pg = require("pg");
-var conString = "postgres://zfaagftogdvhjz:pcXlJD1bP9AygIM7ivINuDOHvS@ec2-184-73-194-196.compute-1.amazonaws.com:5432/dfcvk500ed0il4"
-    var client = new pg.Client(conString);
+var conString = "postgres://zfaagftogdvhjz:pcXlJD1bP9AygIM7ivINuDOHvS@ec2-184-73-194-196.compute-1.amazonaws.com:5432/dfcvk500ed0il4";
+var client = new pg.Client(conString);
 client.connect();
 
 
@@ -15,20 +15,29 @@ app.get('/', function(req, res) {
 	res.send('Hello World!');
     });
 app.get('/read', function(req, res) {
-	
+	var buff = "";
 	//	res.send('A Reader will go here');
 	var query = client.query('SELECT * FROM GolfRounds');
 	
 	query.on('row', function(row) {
-		res.send(JSON.stringify(row));
+		buff = (buff + JSON.stringify(row));
 	    });
+	res.send(buff);
 	
     });
+app.get('/write', function(req, res) {
+    
+	res.send('A write page will go here');
 
+
+});
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
 	console.log("Listening on " + port);
     });
+
+
+
 
 //pg.connect(conString, function(err ,client) {
 	//	if(err) throw err;
